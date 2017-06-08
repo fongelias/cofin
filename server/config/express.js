83b390d117a5bc1b1b2ab8e53f454793e16a5da3
fongelias <fongelias@gmail.com>
2017-06-07 21:14:23 -0400

    changed file structure

--- server/config/express.js
+++ server/config/express.js
@@ -7,6 +7,7 @@ module.exports = function() {
 	//Initializes express application
 	var app = express();
 
+	app.use(express.static('public'));
 	//Pass application instance through routing files
 	require('../app/routes/index.server.routes')(app);
 
