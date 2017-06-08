4a7f10f7af0da618612a826eed966c5adfb7f223
fongelias <fongelias@gmail.com>
2017-06-08 17:05:13 -0400

    Finished writing tests for createStore

--- client/app/redux/store/createStore.test.js
+++ client/app/redux/store/createStore.test.js
@@ -4,6 +4,7 @@ import createStore from './createStore';
 describe('A store', () => {
 
 	let store = {};
+	//Test Actions
 	const increment_action = {
 		name: 'increment',
 	}
@@ -41,13 +42,38 @@ describe('A store', () => {
 	describe('#dispatch()', () => {
 		it('should transform the state given an action', () => {
 			store.dispatch(increment_action);
+			asserts(store.getState() == 1);
 		})
 
 		it('should transform the state given a different action', () => {
-
+			store.dispatch(decrement_action);
+			asserts(store.getState() == 0, 'Make sure this test is correct');
 		})
 
 		it('should not change when there is no action', () => {
+			store.dispatch();
+			asserts(store.getState() == 0);
+		})
+	})
+
+	describe('#subscribe()', () => {
+		it('should call a subscribed function when the state changes', () => {
+			let testBool = false;
+			let boolChange = () => testBool = true;
+
+			store.subscribe(boolChange);
+
+			asserts(testBool);
+		})
+
+		it('should remove a subscribed function when called again', () => {
+			let testBool = false;
+			let boolChange = () => testBool = true;
+			
+			store.subscribe(boolChange);
+			store.subscribe(boolChange);
+
+			asserts(testBool);
 
 		})
 	})
