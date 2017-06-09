629346370bfaac525003af1e4fc8302369d66976
fongelias <fongelias@gmail.com>
2017-06-09 15:21:52 -0400

    Restructured file structure to remove combine reducers folder

--- client/app/redux/combineReducers/combineReducers.js
+++
@@ -1,9 +0,0 @@
-export default (reducers) => {
-	return (state = {}, action) => {
-		return Object.keys(reducers).reduce((nextState, key) => {
-			nextState[key] = reducers[key](state[key], action);
-
-			return nextState;
-		}, {});
-	}
-}
