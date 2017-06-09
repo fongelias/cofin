14e82ee879f9eb549cc6d40095aad10b55166132
fongelias <fongelias@gmail.com>
2017-06-08 22:15:13 -0400

    Added and passed tests for combineReducers

---
+++ client/app/redux/combineReducers/combineReducers.js
@@ -0,0 +1,9 @@
+export default (reducers) => {
+	return (state = {}, action) => {
+		return Object.keys(reducers).reduce((nextState, key) => {
+			nextState[key] = reducers[key](state[key], action);
+
+			return nextState;
+		}, {});
+	}
+}
