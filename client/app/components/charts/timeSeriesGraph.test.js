da0d9e998dca83499fbfc770c136a0896a7f53da
fongelias <fongelias@gmail.com>
2017-06-10 23:19:15 -0400

    Added empty test cases for timeSeriesGraph

---
+++ client/app/components/charts/timeSeriesGraph.test.js
@@ -0,0 +1,40 @@
+import assert from 'assert';
+
+
+//import timeSeriesGraph from './timeSeriesGraph';
+
+
+describe('A d3 line graph', () => {
+
+
+	//let line = document.getElementsByTagName('body');
+	//console.log(line);
+	
+	describe('#create()', () => {
+
+		it('should mount the graph', () => {
+			assert();
+		})
+
+		it('should represent its data', () => {
+			assert();
+		})
+	})
+
+	describe('#update()', () => {
+
+		it('should add new data', () => {
+
+		})
+
+		it('should delete old data', () => {
+
+		})
+	})
+
+	describe('#destroy()', () => {
+		it('should destroy the existing graph', () => {
+
+		})
+	})
+})
