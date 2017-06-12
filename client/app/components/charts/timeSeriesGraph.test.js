248e57c7682c903eaa1b6774ac04cc285651d7a1
fongelias <fongelias@gmail.com>
2017-06-11 22:06:05 -0400

    Wrote test for timeSeriesGraph.scale()

--- client/app/components/charts/timeSeriesGraph.test.js
+++ client/app/components/charts/timeSeriesGraph.test.js
@@ -7,8 +7,8 @@ import timeSeriesGraph from './timeSeriesGraph';
 describe('A d3 line graph', () => {
 
 	const props = {
-		height: 10,
-		width: 15,
+		height: 100,
+		width: 200,
 	}
 
 	const dataSet1 = [
@@ -57,12 +57,28 @@ describe('A d3 line graph', () => {
 	})
 
 	describe('#scale()', () => {
+		//Test Data
+		const scaleDataSet = [
+			["2011-12-31", 0],
+			["2012-01-10", 10], //10 days
+		];
+		const testPoint = ["2012-01-05", 5];
 
+		//Tests
 		it('should return a scale function as a property for X', () => {
-			console.log(graph.scale(props, dataSet1));
+			const testDate = new Date(Date.parse(testPoint[0]));
+			const scaledPixel = graph.scale(props, scaleDataSet).x(testDate);
+			const expectedPixel = props.width * 5 / 10;
+
+			assert(scaledPixel == expectedPixel);
 		})
 
 		it('should return a scale function as a property for Y', () => {
+			const testPrice = testPoint[1];
+			const scaledPixel = graph.scale(props, scaleDataSet).y(testPrice);
+			const expectedPixel = props.height * 5 / 10;
+
+			assert(scaledPixel == expectedPixel);
 
 		})
 	})
