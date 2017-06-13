d79bbf758cd44bc7c39fdd565331231df400a07a
fongelias <fongelias@gmail.com>
2017-06-13 09:20:31 -0400

    Added id and element for mounting, and added reactdom.render method for root of app

--- client/app/index.jsx
+++ client/app/index.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider, store, actions } from './redux';
+import { CofinStocks } from './components/containers/cofinStocks.jsx';
 
 //Figure out app Structure
 //Figure out redux module structure
 //d3 redux
 
+ReactDOM.render(
+	<Provider store={store}>
+		<CofinStocks />
+	</Provider>,
+	document.getElementById("root")
+)
