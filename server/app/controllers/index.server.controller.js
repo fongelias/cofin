50f253c5e105b90b9374c71da3a3a77aa7eea257
fongelias <fongelias@gmail.com>
2017-06-14 11:34:03 -0400

    Connected Quandl service to the templated index, and parsed out data into a state object

--- server/app/controllers/index.server.controller.js
+++ server/app/controllers/index.server.controller.js
@@ -1,10 +1,24 @@
+//Dependencies
+var quandl = require('../services/quandl.server.service.js');
+
+//Exports
 exports.renderIndex = function(req, res) {
-	res.render('pages/index', {
-		state: {
-			tickers: ['FB'],
-			prices: {
-				'FB':[],
-			}
-		},
-	})
+	var state = {
+		ticker: [],
+		prices: {},
+	};
+
+
+	quandl.getStockData()
+		.then(function(response) {
+			state.ticker = response.ticker;
+			state.prices[response.ticker] = response.datatable.data;
+
+			res.render('pages/index', {
+				state: state,
+			});
+		}).catch(function(error) {
+			console.log(error);
+		}) 
+
 }
