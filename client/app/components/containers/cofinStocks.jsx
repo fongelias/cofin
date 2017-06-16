416af430bdde08d4f536f67fbfccede96dc86ee2
fongelias <fongelias@gmail.com>
2017-06-16 19:22:22 -0400

    Added and updated page container

---
+++ client/app/components/containers/cofinStocks.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import StockGraph from '../stockGraph/stockGraph.jsx';
+import StockList from '../stockList/stockList.jsx';
+import PropTypes from 'prop-types';
+
+
+
+export default class CofinStocks extends Component {
+	render() {
+		return(
+			<div className="cofinStocks">
+				<StockGraph store={this.context.store}/>
+				<StockList store={this.context.store}/>
+			</div>
+		)
+	}
+}
+
+CofinStocks.contextTypes = {
+	store: PropTypes.object,
+};
